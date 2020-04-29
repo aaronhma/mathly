84780d0de49d328747384ffa28acf927a9ffc95a
Aaron Ma <aaronhma@gmail.com>
2020-04-29 16:10:20 -0700

    Add more things to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 # from .mathly.shared import version
 
 def load_description(readme="README.md"):
     with open(readme) as file:
-        file.read()
+        return file.read()
 
 
 NAME = "Mathly"
@@ -19,9 +20,10 @@ setup(
     author = AUTHOR,
     description = SHORT_DESCRIPTION,
     long_description = LONG_DESCRIPTION,
-    # long_description_content_type = "text/markdown",
+    long_description_content_type = "text/markdown",
     url = "https://github.com/aaronhma/mathly",
     keywords = ["math", "linear algebra", "mathly"],
+    include_package_data = True,
     classifiers = [
         "License :: OSI Approved :: MIT License",
         # "Programming Language :: Python 3.7"
